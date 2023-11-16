efeb28e40db570f6ac25bf164138ffacf2a00dce
TruongAnn2003 <nguyentruongan281203@gmail.com>
2023-11-16 12:11:41 +0700

    an so cute

--- node.py
+++ node.py
@@ -1,24 +1,28 @@
-import pygame
 import numpy as np
-from pygame.locals import *
-import time
-import random
-import math
-from snake import Snake
-from food import Food
 from static import *
+W = WIDTH_BOARD//CELL_SIZE
+H = HEIGHT_BOARD//CELL_SIZE
+NAV_H = 50//CELL_SIZE
 
 class Node:
     def __init__(self,snakeX,snakeY,foodX,foodY):
-        self.matrix= np.zeros((HEIGHT_BOARD//CELL_SIZE,WIDTH_BOARD//CELL_SIZE))
+        self.matrix= np.zeros((H,W),dtype=int)
         self.snakeX = snakeX
         self.snakeY = snakeY
         self.foodX = foodX
         self.foodY = foodY
     def CreateState(self):
-        self.matrix[(self.foodX//CELL_SIZE)-1][(self.foodY//CELL_SIZE)-1]=-1
+        self.matrix[(self.foodY//CELL_SIZE)-1][(self.foodX//CELL_SIZE)-1]=-1
         for i in range(len(self.snakeX)):
-            self.matrix[(self.snakeX[i]//CELL_SIZE)-1][(self.snakeY[i]//CELL_SIZE)-1]=i+1
+            self.matrix[(self.snakeY[i]//CELL_SIZE)-1][(self.snakeX[i]//CELL_SIZE)-1]=i+1
+        self.CreateObstacle()
         return self.matrix
     def out(self):      
         print(self.CreateState())
+    
+    def CreateObstacle(self):
+        self.matrix[:H,0] = -2
+        self.matrix[-1, :W] = -2
+        self.matrix[:H, -1] = -2
+        for i in range(NAV_H):
+            self.matrix[i,:W] = -2
